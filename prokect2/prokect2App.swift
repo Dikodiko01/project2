64fc3637a3c21d3ea51a81a6c3c91300a776b512
dikodiko01 <dikodiko901123@gmail.com>
2022-12-09 15:47:15 +0600

    first vesion

---
+++ prokect2/prokect2App.swift
@@ -0,0 +1,17 @@
+//
+//  prokect2App.swift
+//  prokect2
+//
+//  Created by Diar Orynbek on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct prokect2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
